46698d5b23680be785453cf4c8d2be2766316a52
Riley Coulter <rcoulter35@gmail.com>
2017-07-10 19:32:06 -0400

    Fixed errors in Database class. Working database test (sqlite select statement)

--- server/database.cc
+++ server/database.cc
@@ -1,4 +1,5 @@
 #include "headers/database.h"
+#include "headers/sqlite3.h"
 
 Database::Database(std::string path)
 {		
@@ -9,21 +10,25 @@ Database::Database(std::string path)
 void Database::get_user_by_id(int id)
 {
 	std::string name, password;
-	std::string query_string = std::string("SELECT name, password FROM user WHERE id=") + std::to_string(id) + ";";   
+	
+	std::string query_string = std::string("SELECT name, password FROM user WHERE id=\'") + std::to_string(id) + "\';";   
 	const char *query = (const char *) query_string.c_str(); 
+	
 	sqlite3_stmt *statement; 
 	sqlite3_prepare(database, query, -1, &statement, NULL); 
+	
 	//do stuff with that statement
 	sqlite3_step(statement); 
+	
 	name = std::string((char *)sqlite3_column_blob(statement, NAME_INDEX));
 	password = std::string((char *)sqlite3_column_blob(statement, PASSWORD_INDEX));
+	
 	sqlite3_step(statement); 
 	sqlite3_finalize(statement); 
-	//sqlite3_step to finish... might be unnecessary
-	//sqlite3_finalize statement destroys statement "object"
+	
 	std::cout << name << ": " << password << std::endl; 
-
 }
+
 
 Database::~Database()
 {
--- server/dbtest.cc
+++ server/dbtest.cc
@@ -3,6 +3,6 @@
 int main()
 {
 	Database database(std::string("database.db")); 
-	database.get_user_by_id(0); 
+	database.get_user_by_id(1); 
 }
 
