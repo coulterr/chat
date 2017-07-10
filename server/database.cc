bc6e56249eb9b08c06c39d38c389dba821faaaf1
Riley Coulter <rcoulter35@gmail.com>
2017-07-10 10:03:06 -0400

    outline for datatbase statement handling

--- server/database.cc
+++ server/database.cc
@@ -10,6 +10,10 @@ User Database::get_user_by_id(std::string id)
 	sqlite3_stmt *statement; 
 	sqlite3_prepare(database, query, -1, &statement, NULL); 
 	//do stuff with that statement
+	//sqlite3_step to evaluate up to single row
+	//sqlite3_column multiple times for user fields
+	//sqlite3_step to finish... might be unnecessary
+	//sqlite3_finalize statement destroys statement "object"
 
 
 }
